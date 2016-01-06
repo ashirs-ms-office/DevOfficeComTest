001c0534231a5d1d193293a4c8e201da33eaf99e
ChunLi0819 <v-chli@microsoft.com>
2016-01-06 10:21:58 +0800

    Add structure.cs

---
+++ TestFramework/DataStructure/Structures.cs
@@ -0,0 +1,23 @@
+﻿namespace TestFramework
+{
+    /// <summary>
+    /// This struct contains the searched result's information
+    /// </summary>
+    public struct SearchedResult
+    {
+        /// <summary>
+        /// The result name
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// The result description
+        /// </summary>
+        public string Description;
+        
+        /// <summary>
+        /// The count of views
+        /// </summary>
+        public long ViewCount;
+    }
+}
