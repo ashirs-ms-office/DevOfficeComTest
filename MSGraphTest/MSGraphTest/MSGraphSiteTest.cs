3130f878ea2081d4c6c8f6b57a1a2d450672144b
ChunLi0819 <v-chli@microsoft.com>
2016-02-19 13:46:32 +0800

    Add test cases to verify wether the site can be accessed by search engines

---
+++ MSGraphTest/MSGraphTest/MSGraphSiteTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Net;
+using TestFramework;
+
+namespace MSGraphTest
+{
+    /// <summary>
+    /// Site Test class for Microsoft Graph
+    /// </summary>
+    [TestClass]
+    public class MSGraphSiteTest
+    {
+        #region Additional test attributes
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            GraphBrowser.Close();
+        }
+        #endregion
+
+        /// <summary>
+        /// Verify whether robots.txt specifies the site is accessible.
+        /// </summary>
+        [TestMethod]
+        public void BVT_Graph_S06_TC01_CanAccessSiteRobots()
+        {
+            string url = GraphBrowser.BaseAddress + "/robots.txt";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream receiveStream = response.GetResponseStream();
+            StreamReader readStream = new StreamReader(receiveStream);
+            string readResponse = readStream.ReadToEnd();
+            bool disallowed = readResponse.Contains("Disallow:");
+            Assert.IsTrue(disallowed, "The site should not be allowed to access");
+        }
+
+        // <summary>
+        /// Verify whether robots.txt of production site specifies the site is accessible.
+        /// </summary>
+        [TestMethod]
+        public void BVT_Graph_S06_TC02_CanAccessProductionSiteRobots()
+        {
+            string url = "http://graph.microsoft.io/robots.txt";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream receiveStream = response.GetResponseStream();
+            StreamReader readStream = new StreamReader(receiveStream);
+            string readResponse = readStream.ReadToEnd();
+            bool disallowed = readResponse.Contains("Disallow:");
+            bool allowed = readResponse.Contains("Allow:");
+            Assert.IsTrue(!disallowed && allowed, "The site should be allowed to access");
+        }
+    }
+}
