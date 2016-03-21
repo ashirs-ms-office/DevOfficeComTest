59b7df47bdc171059ea66d67166245811a8e2c26
ChunLi0819 <v-chli@microsoft.com>
2016-03-21 17:46:39 +0800

    Adjust the test result format as a table. Remove priority High when all cases passed

--- SearchEngineTest/Script/GetConfigFileNode.ps1
+++ SearchEngineTest/Script/GetConfigFileNode.ps1
@@ -1,16 +1,27 @@
 Param(
-    [string]$nodeName
+    [string]$nodeName,
+    #If the follwoing is set as true, read the node from App.config, 
+    #else, from AutoTestConfig.xml
+    [bool]$fromAppConfig=$false
     )
  #   write-host $nodeName
 #----------------------------------------------------------------------------
 # Get the value of the node
 #----------------------------------------------------------------------------       
 [xml]$configContent = New-Object XML
-$configContent.Load(".\AutoTestConfig.xml")
-$propertyNodes = $configContent.GetElementsByTagName("Property")
+if($fromAppConfig -eq $true)
+{
+    $configContent.Load("..\SearchEngineTest\App.config")
+    $propertyNodes = $configContent.GetElementsByTagName("add")
+}
+else
+{
+    $configContent.Load(".\AutoTestConfig.xml")
+    $propertyNodes = $configContent.GetElementsByTagName("Property")
+}
 foreach($node in $propertyNodes)
 {
-    if($node.GetAttribute("name") -eq $nodeName)
+    if($node.GetAttribute("key") -eq $nodeName)
     {
         $value=$node.GetAttribute("value")
        #write-host $value
